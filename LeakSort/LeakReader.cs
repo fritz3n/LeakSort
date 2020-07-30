a1f05d3c58522062a8869852935471f116c7901d
TheHllm <hllmollenkopf@gmail.com>
2020-07-30 16:10:47 +0200

    Faster lookups

--- LeakSort/LeakReader.cs
+++ LeakSort/LeakReader.cs
@@ -4,32 +4,65 @@ using System.Threading.Tasks;
 
 namespace LeakSort
 {
-	internal class LeakReader
-	{
-		const string legalCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
+    internal class LeakReader
+    {
+        const string legalCharacters = "abcdefghijklmnopqrstuvwxyz0123456789";
 
-		private int layers;
-		private string basePath;
+        private int layers;
+        private string basePath;
 
-		public LeakReader(int layers, string basePath)
-		{
-			this.layers = layers;
-			this.basePath = basePath;
-		}
+        public LeakReader(int layers, string basePath)
+        {
+            this.layers = layers;
+            this.basePath = basePath;
+        }
 
-		public async Task<IEnumerable<string>> Lookup(string data)
-		{
-			string path = LeakSaver.GetFilePath(basePath, data, layers);
-			LinkedList<string> items = new LinkedList<string>();
-			using (StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
-			{
-				string line = await sr.ReadLineAsync();
-				if (line.StartsWith(data))
-				{
-					items.AddLast(line);
-				}
-			}
-			return items;
-		}
-	}
+        public async Task<IEnumerable<string>> Lookup(string data)
+        {
+            string path = LeakSaver.GetFilePath(basePath, data, layers);
+            LinkedList<string> items = new LinkedList<string>(); 
+            try
+            {
+                using (StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = await sr.ReadLineAsync();
+                        if (line.StartsWith(data))
+                        {
+                            items.AddLast(line);
+                        }
+                    }
+                }
+                return items;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return new LinkedList<string>();
+            }
+        }
+        public async Task<bool> HasResult(string data)
+        {
+            string path = LeakSaver.GetFilePath(basePath, data, layers);
+            try
+            {
+                using (StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open, FileAccess.Read)))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = await sr.ReadLineAsync();
+                        if (line.StartsWith(data))
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return false;
+            }
+        }
+    }
 }
